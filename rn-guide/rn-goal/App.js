39d5d7d5737790a04b2c637e5cf6aff84a3ad817
Hudayfe Yurt <hudayfeyurt@hotmail.com>
2021-05-09 00:44:02 +0300

    goal

---
+++ rn-guide/rn-goal/App.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import {StyleSheet, View, FlatList, Button} from 'react-native';
+import uuid from 'react-native-uuid';
+import GoalItem from "./src/components/GoalItem";
+import GoalInput from "./src/components/GoalInput";
+
+export default function App() {
+    const [courseGoals, setCourseGoals] = useState([]);
+    const [isAddMode, setIsAddMode] = useState(false);
+
+    const addGoalHandler = goalTitle => {
+        if (goalTitle.length === 0) {
+            return;
+        }
+        setCourseGoals(currentGoals => [
+            ...currentGoals,
+            {id: uuid.v4(), value: goalTitle}
+        ]);
+        setIsAddMode(false);
+    };
+
+    const removeGoalHandler = goalId => {
+        setCourseGoals(currentGoals => {
+            return currentGoals.filter((goal) => goal.id !== goalId);
+        });
+    };
+
+    const cancelGoalAdditionHandler = () => {
+        setIsAddMode(false);
+    };
+
+    return (
+        <View style={styles.screen}>
+            <Button title='Add New Goal' onPress={() => setIsAddMode(true)}/>
+            <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalAdditionHandler}/>
+            <FlatList
+                keyExtractor={(item, index) => item.id}
+                data={courseGoals}
+                renderItem={itemData => (
+                    <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value}/>
+                )}/>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        padding: 50,
+    },
+});
